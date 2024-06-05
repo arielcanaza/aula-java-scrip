fcf0d2a3a1c15fbc60590b6e89666ac83afb2b29
ariel <canaza23.ac@outlook.com>
2024-06-04 21:39:45 -0300

    aulas java/scrip
    continuação

---
+++ aula8.js
@@ -0,0 +1,8 @@
+const texto1 = 'Aqui temos uma string normal ';
+
+const texto2 = "já esta, apesar das aspas duplas ao inves das simples tambem e uma string igual ";
+
+const numeroRegistro =  31472;
+const stringTemplate = `O número que quero registrar é o ${numeroRegistro}` ;
+
+console.log(stringTemplate);
---
+++ aula9.js
@@ -0,0 +1,12 @@
+const grupoNumber = [azul, amarelo, vermelho, preto, branco];
+console.log(grupoNumber);
+
+console.log(`o valor do elemento da minha lista é ${grupoNumber[0]}`);
+
+console.log(`O valor do elemento da minha lista é ${grupoNumber[1]}`);
+
+console.log(`O valor do elemento da minha lista é ${grupoNumber[2]}`);
+
+console.log(`O valor do elemento da minha lista é ${grupoNumber[3]}`);
+
+console.log(`O valor do elemento da minha lista é ${grupoNumber[4]}`);
